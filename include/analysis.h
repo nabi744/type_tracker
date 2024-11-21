ecbfffeefd0bfb27f7df9497e5039b0e691a215e
chuzzy <128759526+chuzzyhuman@users.noreply.github.com>
2024-11-21 07:41:23 +0000

    words_per_min, calculate_accuracy

--- include/analysis.h
+++ include/analysis.h
@@ -1,1 +1,31 @@
-#
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+double words_per_min(char *text, double time){
+    int words = 0;
+    int i = 0;
+    while(text[i] != '\0'){
+        if(isspace(text[i])){
+            words++;
+        }
+        i++;
+    }
+    return words/time;
+}
+
+double calculate_accuracy(char *text, char *input){
+    int i = 0;
+    int correct = 0;
+    int total = 0;
+    while(text[i] != '\0'){
+        if(text[i] == input[i]){
+            correct++;
+        }
+        total++;
+        i++;
+    }
+    return (double)correct/total;
+}
